10de2506143baade86c4f2b18651d9312d2ca2be
Yash <ys136450@gmail.com>
2026-06-30 16:45:47 +0530

    Some Fixess

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import app from "./app.js";
 import dotenv from "dotenv";
 
-dotenv.config({
-    path: './.env'
-});
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>{
+// Load .env FIRST
+dotenv.config();
+
+import app from "./app.js";
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
     console.log(`Server is running on port ${PORT}`);
-})
+
+});
